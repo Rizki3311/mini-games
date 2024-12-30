3ba1fe91f783d75c0566b6e7f2232611ab3580e8
rizki nugraha <88242195+Rizki3311@users.noreply.github.com>
2024-12-30 17:44:40 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,16 @@
+// //string
+
+// //const a = 'ini string';
+
+// // console.log(a);
+
+// // number
+
+// const a = 202.3;
+// console.log(a);
+
+//
+
+for (let i = 0; i < 10000000; i++){
+    console.log('ini nilai i : ', i);
+}
